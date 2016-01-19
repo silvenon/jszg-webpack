77d22cdadb1b9fde6febc0a65bb144130a57a826
Matija Marohnić <matija.marohnic@gmail.com>
2016-01-19 22:27:24 +0100

    Add React

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ import './foo';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'octicons/octicons/octicons.css';
 import './base.css';
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+  <div className="alert alert-success">
+    <strong>{'Success!'}</strong> {'Hello from React!'}
+  </div>,
+  document.getElementById('react')
+);
